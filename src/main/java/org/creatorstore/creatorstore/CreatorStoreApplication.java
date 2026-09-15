53c75faf1627ab529b612387708bddaf005282f3
sehan001 <nmsehan.001@gmail.com>
2026-09-16 01:08:25 +0530

    added .env configuration

--- src/main/java/org/creatorstore/creatorstore/CreatorStoreApplication.java
+++ src/main/java/org/creatorstore/creatorstore/CreatorStoreApplication.java
@@ -1,5 +1,6 @@
 package org.creatorstore.creatorstore;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CreatorStoreApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv=Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach((entry)->System.setProperty(
+                entry.getKey(),entry.getValue()
+        ));
+
+
         SpringApplication.run(CreatorStoreApplication.class, args);
+
+
     }
 
 }
