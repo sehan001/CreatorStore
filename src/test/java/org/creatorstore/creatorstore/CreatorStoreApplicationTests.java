4672b855dbca6acefe6aaa267fb8d88f768d72f8
sehan001 <nmsehan.001@gmail.com>
2026-09-15 21:59:36 +0530

    project setup

---
+++ src/test/java/org/creatorstore/creatorstore/CreatorStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.creatorstore.creatorstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
